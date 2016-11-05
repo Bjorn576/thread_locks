5914447f3dd2dc21bcbe90639471baf568e623cd
Bjorn <jkerssen@sfu.ca>
2016-11-05 15:22:14 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,186 @@
+#include "main.h"
+#include <string.h>
+
+void *fnC()
+{
+    int i;
+    for(i=0;i<1000000;i++)
+    {   
+        c++;
+    }   
+}
+
+
+pthread_mutex_t count_mutex;
+
+
+void *pthreadMutexTest()
+{
+	
+    int i;
+	int j;
+	int k;
+	
+	int localCount = 0;
+	
+    for(i=0;i<numItterations;i++)
+    {
+		
+		for(j=0;j<workOutsideCS;j++)/*How much work is done outside the CS*/
+		{
+			localCount++;
+		}
+		
+		pthread_mutex_lock(&count_mutex);
+		for(k=0;k<workInsideCS;k++)/*How much work is done inside the CS*/
+		{
+			c++;
+		}
+		pthread_mutex_unlock(&count_mutex);    
+	
+    }   
+
+
+}
+
+int runTest(int testID)
+{
+
+/*You must create all data structures and threads for you experiments*/
+
+if (testID == 0 || testID == 1 ) /*Pthread Mutex*/
+{
+	c=0;
+	struct timespec start;
+	struct timespec stop;
+	unsigned long long result; //64 bit integer
+
+	pthread_t *threads = (pthread_t* )malloc(sizeof(pthread_t)*numThreads);	
+	int i;
+	int rt;
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	for(i=0;i<numThreads;i++)
+	{
+	
+	   if( rt=(pthread_create( threads+i, NULL, &pthreadMutexTest, NULL)) )
+	   {
+		    printf("Thread creation failed: %d\n", rt);
+		    return -1;	
+	   }
+	
+	}
+	
+	for(i=0;i<numThreads;i++) //Wait for all threads to finish
+	{
+		 pthread_join(threads[i], NULL);
+	}
+	clock_gettime(CLOCK_MONOTONIC, &stop);
+
+	printf("Threaded Run Pthread (Mutex) Total Count: %d\n", c);
+	result=timespecDiff(&stop,&start);
+	printf("Pthread Mutex time(ms): %llu\n",result/1000000);
+
+}
+
+if(testID == 0 || testID == 2) /*Pthread Spinlock*/
+{
+  printf("Testing SpinLock...\n");
+  my_spinlock_t lock;
+  my_spinlock_init(&lock);
+  
+  my_spinlock_lockTAS(&lock);
+  printf("Done!\n");
+}
+
+if(testID == 0 || testID == 3) /*MySpinlockTAS*/
+{
+/* mySpinlock TAS goes here*/
+}
+
+/*....you must implement the other tests....*/
+
+	return 0;
+}
+
+int testAndSetExample()
+{
+volatile long test = 0; //Test is set to 0
+printf("Test before atomic OP:%d\n",test);
+tas(&test);
+printf("Test after atomic OP:%d\n",test);
+}
+
+int processInput(int argc, char *argv[])
+{
+
+/*testid: 0=all, 1=pthreadMutex, 2=pthreadSpinlock, 3=mySpinLockTAS, 4=mySpinLockTTAS, 5=myMutexTAS, 6=myQueueLock*/
+	/*You must write how to parse input from the command line here, your software should default to the values given below if no input is given*/
+  int i;
+  int tflag, iflag, oflag, cflag, dflag;
+  int numThreads, numItterations, OperationsOutsideCS, OperationsInsideCS, testID;
+  tflag = iflag = oflag = cflag = dflag = 0;
+  
+  //Custom input values
+  for (i=1;i<argc;i++)
+  {
+    if(!strcmp("-t", argv[i]) && i+1 < argc-1)
+    {
+      numThreads = atoi(argv[i+1]);
+      tflag = 1;
+    }
+    else if(!strcmp("-i", argv[i]) && i+1 < argc-1)
+    {
+      numItterations = atoi(argv[i+1]);
+      iflag = 1;
+    }
+    else if(!strcmp("-o", argv[i]) && i+1 < argc-1)
+    {
+      OperationsOutsideCS = atoi(argv[i+1]);
+      oflag = 1;
+    }
+    else if(!strcmp("-c", argv[i]) && i+1 < argc-1)
+    {
+      OperationsInsideCS = atoi(argv[i+1]);
+      cflag = 1;
+    }
+    else if(!strcmp("-d", argv[i]) && i+1 < argc-1)
+    {
+      testID = atoi(argv[i+1]);
+      dflag = 1;
+    }
+  }
+  
+  //Set default values if necessary
+  if(!tflag)
+    numThreads = 4;
+  if(!iflag)
+    numItterations = 100000;
+  if(!dflag)
+    testID = 2;
+  if(!oflag)
+    workOutsideCS = 0;
+  if(!cflag)
+    workInsideCS = 1;
+  
+  printf("Number of threads is: %d\n", numThreads);
+  
+
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+
+
+	printf("Usage of: %s -t #threads -i #Itterations -o #OperationsOutsideCS -c #OperationsInsideCS -d testid\n", argv[0]);
+	printf("testid: 0=all, 1=pthreadMutex, 2=pthreadSpinlock, 3=mySpinLockTAS, 4=mySpinLockTTAS, 5=myMutexTAS, 6=myQueueLock, \n");	
+	
+	//testAndSetExample(); //Uncomment this line to see how to use TAS
+	
+	processInput(argc,argv);
+	runTest(testID);
+	return 0;
+
+}
---
+++ sync.c
@@ -0,0 +1,100 @@
+/*
+ * NAME, etc.
+ *
+ * sync.c
+ *
+ */
+
+#define _REENTRANT
+
+#include "sync.h"
+#include <stdlib.h>
+
+
+/*
+ * Spinlock routines
+ */
+
+int my_spinlock_init(my_spinlock_t *lock)
+{
+  if(lock != NULL)
+  {
+    lock->val = 0;
+    return 1;
+  }
+  return 0;
+}
+
+int my_spinlock_destroy(my_spinlock_t *lock)
+{
+}
+
+int my_spinlock_unlock(my_spinlock_t *lock)
+{
+  
+}
+
+int my_spinlock_lockTAS(my_spinlock_t *lock)
+{
+  while(tas(&(lock->val)));
+  printf("Locked!\n");
+}
+
+
+int my_spinlock_lockTTAS(my_spinlock_t *lock)
+{
+}
+
+int my_spinlock_trylock(my_spinlock_t *lock)
+{
+}
+
+
+/*
+ * Mutex routines
+ */
+
+int my_mutex_init(my_mutex_t *lock)
+{
+}
+
+int my_mutex_destroy(my_mutex_t *lock)
+{
+}
+
+int my_mutex_unlock(my_mutex_t *lock)
+{
+}
+
+int my_mutex_lock(my_mutex_t *lock)
+{
+}
+
+int my_mutex_trylock(my_mutex_t *lock)
+{
+}
+
+/*
+ * Queue Lock
+ */
+
+int my_queuelock_init(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_destroy(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_unlock(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_lock(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_trylock(my_queuelock_t *lock)
+{
+}
+
